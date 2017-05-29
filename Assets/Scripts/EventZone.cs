50b85d5b2a269656e327e919448171d38a675663
kanghyun-PC\kanghyun <leeganghyeon422@naver.com>
2017-05-29 16:38:09 +0900

    1. 스크립트 추가 EventZone

---
+++ Assets/Scripts/EventZone.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class EventHolder
+{
+    public UnityEvent _event;
+    public bool useOnce;
+    private bool close;
+
+    public void SetClose()
+    {
+        close = true;
+    }
+    public bool IsAvailable()
+    {
+        if (close == true)
+            return false;
+        else
+            return true;
+    }
+}
+
+
+public class EventZone : MonoBehaviour {
+
+    public EventHolder[] enterEvent;
+    public float stayTick;
+    public EventHolder[] stayEvent;
+    public EventHolder[] exitEvent;
+    private bool isEvent;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        foreach(EventHolder e in enterEvent)
+        {
+            if(e.IsAvailable())
+            {
+                if (e.useOnce)
+                    e.SetClose();
+                e._event.Invoke();
+            }
+        }
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (!isEvent)
+        {
+            isEvent = false;
+            StartCoroutine(Delay(stayTick));
+            foreach (EventHolder e in stayEvent)
+            {
+                if (e.IsAvailable())
+                {
+                    if (e.useOnce)
+                        e.SetClose();
+                    e._event.Invoke();
+                }
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        foreach (EventHolder e in exitEvent)
+        {
+            if (e.IsAvailable())
+            {
+                if (e.useOnce)
+                    e.SetClose();
+                e._event.Invoke();
+            }
+        }
+    }
+
+    IEnumerator Delay(float tick)
+    {
+        yield return new WaitForSeconds(tick);
+        isEvent = true;
+    }
+
+}
